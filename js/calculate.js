90363b2d7b3fd8deab6e2243cdb1b5700e3c8455
Hall <vhall204@cable.comcast.com>
2019-06-03 23:12:13 -0400

    Can now display input text on site

---
+++ js/calculate.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("#result").click(function() {
+  //Gets equation
+  var equation = document.getElementById("equation").value;
+    alert("Your equation is: " + equation);
+  });
+
+});
+
+
